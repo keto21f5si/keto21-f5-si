1dde405525059927810495f1e6e0565b29b7f1ab
keto21f5si <keto21.abc@gmail.com>
2024-12-14 16:39:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME "";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./keto.svg"
+];
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  })
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(async function() {
+    try {
+      if (event.request.url.startWith("http")) {
+        let res = await fetch(event.request);
+        let cache = await cache.open(CACHE_NAME);
+        cache.put(event.request.url, res.clone());
+        return res;
+      }
+      return fetch(event.request);
+    }
+    catch (error) {
+      return cache.match(event.request);
+    }
+  }());
+});
